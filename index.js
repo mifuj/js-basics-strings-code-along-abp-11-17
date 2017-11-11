f6e34cab6f211949d1050a752053dcf4f2688638
Miles Fujimoto <milesfujimoto@gmail.com>
2017-11-11 02:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const currentUser = 'Miles Fujimto';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const currentUser = 'Edsger Dijkstra';
+ 
+...
+ 
+const shortGreeting = ... ;
+ 
+shortGreeting;
