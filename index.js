fbbe78a25860577303215f99a5980420c6d3aa07
Miles Fujimoto <milesfujimoto@gmail.com>
2017-11-11 02:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = 'Miles Fujimto';
-const welcomeMessage = 'Welcome to Flatbook, + currentUser';
+const welcomeMessage = 'Welcome to Flatbook, currentUser';
